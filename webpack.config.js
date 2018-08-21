0c15be4d9a6df6b3f45697e4912873f40bda40da
lukaVarga <varga.luka93@gmail.com>
2018-08-21 22:54:26 +0200

    build: Introduce separation between build and dist
    Introduce separation of javascript-version that is in the build folder and production-runtime
    minified js file for website use only. Introduce related script changes, config and gitignore
    changes for handling the builds

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/runtime/index.runtime.ts',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: [{
+					loader: 'ts-loader',
+					options: {
+						configFile : 'tsconfig.dist.json'
+					}
+				}],
+				include: [
+					path.resolve(__dirname, 'src', 'runtime')
+				],
+				exclude: [
+					/node_modules/,
+					path.resolve(__dirname, 'src', 'buildtime'),
+					path.resolve(__dirname, '__tests__')
+				]
+			}
+		]
+	},
+	resolve: {
+		extensions: [ '.tsx', '.ts', '.js' ]
+	},
+	output: {
+		filename: 'OptimusIMG-bundle.min.js',
+		path: path.resolve(__dirname, 'dist'),
+		library: 'OptimusIMG'
+	}
+};
