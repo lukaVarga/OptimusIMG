21890ce5efc0ac07b39252094c921e5457c688d2
lukaVarga <varga.luka93@gmail.com>
2019-08-23 20:17:08 +0200

    feat(webp): Provide a helper function that detects if the browser supports webp format

--- __tests__/runtime/helpers.test.ts
+++ __tests__/runtime/helpers.test.ts
@@ -1,4 +1,6 @@
-import {loadImageInBackground} from '../../src/runtime/helpers';
+import {IOptimusWindowCache, loadImageInBackground, supportsWebp} from '../../src/runtime/helpers';
+
+declare const window: IOptimusWindowCache & Window;
 
 describe('Helpers', () => {
     describe('loadImageInBackground', () => {
@@ -30,6 +32,58 @@ describe('Helpers', () => {
         test('it tries to load image in background and rejects promise if an error occurs', (done: any) => {
             loadImageInBackground('src').catch((result: boolean) => {
                 expect(result).toBe('OptimusIMG: could not load image src in background');
+                done();
+            });
+
+            setTimeout(() => {
+                imageMock.dispatchEvent(new Event('error'));
+            });
+        });
+    });
+
+    describe('supportsWebp', () => {
+        let imageMock: any;
+        let imageSpy: any;
+
+        beforeEach(() => {
+            delete window.optimus__supportsWebp;
+
+            imageMock = new Image();
+
+            // @ts-ignore
+            imageSpy = jest.spyOn(global, 'Image').mockImplementationOnce(() => imageMock);
+        });
+
+        afterEach(() => {
+            imageSpy.mockRestore();
+        });
+
+        test('it returns the cached result stored on window, if present', async () => {
+            window.optimus__supportsWebp = true;
+            expect(await supportsWebp()).toBe(true);
+
+            window.optimus__supportsWebp = false;
+            expect(await supportsWebp()).toBe(false);
+        });
+
+        test('browser supports webp if image can be loaded in background', async (done: any) => {
+            supportsWebp().then((isSupported: boolean) => {
+                expect(isSupported).toBe(true);
+                expect(window.optimus__supportsWebp).toBe(true);
+
+                done();
+            });
+
+            setTimeout(() => {
+                imageMock.dispatchEvent(new Event('load'));
+            });
+        });
+
+        test('browser does not support webp if the image cannot be loaded in background', async (done: any) => {
+            supportsWebp().then((isSupported: boolean) => {
+                expect(isSupported).toBe(false);
+                expect(window.optimus__supportsWebp).toBe(false);
+
                 done();
             });
 
--- src/runtime/helpers.ts
+++ src/runtime/helpers.ts
@@ -1,5 +1,11 @@
 import {consoleMessage} from './helpers/console.helpers';
 
+export interface IOptimusWindowCache {
+    optimus__supportsWebp?: boolean;
+}
+
+declare const window: IOptimusWindowCache & Window;
+
 export function loadImageInBackground(url: string): Promise<boolean | string> {
     return new Promise((resolve: (bool: true) => void, reject: (error: string) => void): void => {
         const IMAGE: HTMLImageElement = new Image();
@@ -23,5 +29,23 @@ export function loadImageInBackground(url: string): Promise<boolean | string> {
         IMAGE.addEventListener('error', handleOnError);
 
         IMAGE.src = url;
+    });
+}
+
+export async function supportsWebp(): Promise<boolean> {
+    // Provide caching mechanism
+    if (typeof window.optimus__supportsWebp === 'boolean') {
+        return window.optimus__supportsWebp;
+    }
+
+    // Base64 representation of a white point image
+    const WEBP_BASE64: string = 'data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoCAAEAAQAcJaQAA3AA/v3AgAA=';
+
+    return await loadImageInBackground(WEBP_BASE64).then(() => {
+        window.optimus__supportsWebp = true;
+        return true;
+    }, () => {
+        window.optimus__supportsWebp = false;
+        return false;
     });
 }
